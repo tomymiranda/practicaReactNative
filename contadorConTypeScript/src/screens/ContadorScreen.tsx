504da1d0c54935e06a6a2a01e6cbbdcaf0baf91d
TomyMiranda <tomasmiranda98@hotmail.com>
2022-05-31 10:08:09 -0300

    se agregga styleSheet

--- contadorConTypeScript/src/screens/ContadorScreen.tsx
+++ contadorConTypeScript/src/screens/ContadorScreen.tsx
@@ -1,34 +1,42 @@
-/* eslint-disable react-native/no-inline-styles */
-/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles *//* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 const ContadorScreen = () => {
     const [contador, setContador] = useState(10);
 
     return (
       //con flex:1 se hace que el contenedor ocupa todo el espacio disponible
       // position relative es el defecto de los componentes
+      //el styles es como css, mejora la visualizacion
       <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-        }}>
+        style={styles.container}>
         <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 46,
-            top: -10,
-          }}>
+          style={styles.title}>
           El valor del contador es: {contador}
         </Text>
-        <Button
-          onPress={(() => setContador( contador + 1))}
-          title="Contar"
-          color="#841584"
-        />
+        <TouchableOpacity onPress={() => setContador(contador + 1)}>
+            <View style={styles.botonIncrementar}>
+            <Text>Aumentar</Text>
+
+            </View>
+        </TouchableOpacity>
       </View>
     );
 };
 export default ContadorScreen;
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 46,
+    top: -10,
+  },
+  botonIncrementar: {
+    backgroundColor: '#00f',
+    borderRadius: 100,
+  },
+});
