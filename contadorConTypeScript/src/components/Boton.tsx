82f32bb191d27988160ccf308e6da87e639d9ef7
TomyMiranda <tomasmiranda98@hotmail.com>
2022-06-01 10:54:12 -0300

    se genera bien el boton

--- contadorConTypeScript/src/components/Boton.tsx
+++ contadorConTypeScript/src/components/Boton.tsx
@@ -1,7 +1,70 @@
-import React from 'react'
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {Text, View, StyleSheet, TouchableNativeFeedback} from 'react-native';
 
-export const Boton = () => {
-  return (
-    <div>Boton</div>
-  )
+// el signo de pregunta al lado del nombre props es para indicar q es opcional
+interface Props {
+  title: string;
+  position?:'left'|'right';
+  onPress: () => void;
 }
+
+export const Boton = ({title, onPress, position = 'right'}: Props) => {
+  return (
+    /*cuando se usa el boton TouchableNativeFeedback, para ajustar su ubicacion
+      se debe poner dentro de una etiqueta <View></View> y pasarle el style 
+      que contiene la ubicacion
+      */
+    <View
+      style={[
+        styles.botonPosition,
+        position === 'left' ? styles.left : styles.right,
+      ]}>
+      <TouchableNativeFeedback
+        onPress={onPress}
+        background={TouchableNativeFeedback.Ripple('black',false,30)}>
+        <View style={styles.botonStyle}>
+          <Text style={styles.textBoton}>{title}</Text>
+        </View>
+      </TouchableNativeFeedback>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  botonStyle: {
+    backgroundColor: '#5856D6',
+    borderRadius: 100,
+    width: 60,
+    height: 60,
+    //el justifyContent: 'center' hace que el hijo( o componente
+    // que esta dentro o texto) se centre respecto a el.
+    justifyContent: 'center',
+    //para generar sombra https://ethercreative.github.io/react-native-shadow-generator/
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+
+    elevation: 24,
+  },
+  textBoton: {
+    color: 'white',
+    //el alignItems: 'center' hace que el texto se centre respecto a el.
+    alignSelf: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  botonPosition: {
+    position: 'absolute',
+    bottom: 24,
+  },
+  right: {
+    right: 24,
+  },
+  left: {
+    left: 24,
+  },
+});
--- contadorConTypeScript/src/screens/ContadorScreen.tsx
+++ contadorConTypeScript/src/screens/ContadorScreen.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-native/no-inline-styles *//* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import { Boton } from '../components/Boton';
 const ContadorScreen = () => {
     const [contador, setContador] = useState(10);
 
@@ -10,20 +11,17 @@ const ContadorScreen = () => {
       //el styles es como css, mejora la visualizacion
       <View style={styles.container}>
         <Text style={styles.title}>El valor del contador es: {contador}</Text>
-        <TouchableOpacity
-          style={styles.botonIncrementarPosicion}
-          onPress={() => setContador(contador + 1)}>
-          <View style={styles.botonIncrementar}>
-            <Text style={styles.textBotonIncrementar}>+1</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.botonDecrementarPosicion}
-          onPress={() => setContador(contador - 1)}>
-          <View style={styles.botonDecrementar}>
-            <Text style={styles.textBotonDecrementar}>-1</Text>
-          </View>
-        </TouchableOpacity>
+        <Boton
+          title="+1"
+          onPress={ () => setContador(contador + 1)}
+          position="right"
+        />
+
+        <Boton
+          title="-1"
+          onPress={ () => setContador(contador - 1)}
+          position="left"
+        />
       </View>
     );
 };
@@ -38,47 +36,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 46,
     top: -10,
-  },
-  botonIncrementar: {
-    backgroundColor: '#5856D6',
-    borderRadius: 100,
-    width: 60,
-    height: 60,
-    //el justifyContent: 'center' hace que el hijo( o componente
-    // que esta dentro o texto) se centre respecto a el.
-    justifyContent: 'center',
-  },
-  textBotonIncrementar: {
-    color: 'white',
-    //el alignItems: 'center' hace que el texto se centre respecto a el.
-    alignSelf: 'center',
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-  botonIncrementarPosicion: {
-    position: 'absolute',
-    bottom: 24,
-    right: 24,
-  },
-  botonDecrementarPosicion: {
-    position: 'absolute',
-    bottom: 24,
-    left: 24,
-  },
-  botonDecrementar: {
-    backgroundColor: '#5856D6',
-    borderRadius: 100,
-    width: 60,
-    height: 60,
-    //el justifyContent: 'center' hace que el hijo( o componente
-    // que esta dentro o texto) se centre respecto a el.
-    justifyContent: 'center',
-  },
-  textBotonDecrementar: {
-    color: 'white',
-    //el alignItems: 'center' hace que el texto se centre respecto a el.
-    alignSelf: 'center',
-    fontSize: 30,
-    fontWeight: 'bold',
   },
 });
